529783d0d26fa90909b4164d127e7621e16a80b7
Hunterbeachum <hsbeachum@my.waketech.edu>
2024-04-20 20:52:07 -0400

    Add infinite scrolling to background

--- 3dbg.gd
+++ 3dbg.gd
@@ -1,11 +1,11 @@
 extends Node3D
-var tilt : String = "top_down"
+var tilt : String = "low"
 var scroll_command : String = "mid"
 var scroll_speed : Vector3 = Vector3.ZERO
-const SCROLL_SPEED_LOW = Vector3(-0.1 * delta, 0.0, 0.0)
-const SCROLL_SPEED_MID = Vector3(-0.2 * delta, 0.0, 0.0)
-const SCROLL_SPEED_HIGH = Vector3(-0.4 * delta, 0.0, 0.0)
-const ANGLE_LOW = Vector3(50.0, 0.0, 0.0)
+const SCROLL_SPEED_LOW = -0.1
+const SCROLL_SPEED_MID = -0.2
+const SCROLL_SPEED_HIGH = -0.4
+const ANGLE_LOW = Vector3(40.0, 0.0, 0.0)
 const ANGLE_TOP = Vector3.ZERO
 @onready var bg_plane : MeshInstance3D = $bg_plane
 @onready var camera : Camera3D = $Camera
@@ -19,16 +19,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if tilt == "top_down":
-		camera.rotation_degrees = camera.rotation_degrees().lerp(ANGLE_TOP, .1)
+		camera.rotation_degrees = camera.rotation_degrees.lerp(ANGLE_TOP, .1)
 	elif tilt == "low":
-		camera.rotation_degrees = camera.rotation_degrees().lerp(ANGLE_LOW, .1)
+		camera.rotation_degrees = camera.rotation_degrees.lerp(ANGLE_LOW, .01)
 	if scroll_command == "low":
-		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_LOW, .1)
+		scroll_speed = scroll_speed.lerp(Vector3(SCROLL_SPEED_LOW * delta, 0.0, 0.0), .1)
 	elif scroll_command == "mid":
-		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_MID, .1)
+		scroll_speed = scroll_speed.lerp(Vector3(SCROLL_SPEED_MID * delta, 0.0, 0.0), .1)
 	elif scroll_command == "high":
-		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_HIGH, .1)
-	bg_plane.transform(scroll_speed)
+		scroll_speed = scroll_speed.lerp(Vector3(SCROLL_SPEED_HIGH * delta, 0.0, 0.0), .1)
+	bg_plane.translate(scroll_speed)
+	# Infinite scrolling by jumping the entire 3D plane UP by 4.0 Y units
+	# (I can't believe this works)
+	if bg_plane.position.y < -2.0:
+		bg_plane.translate(Vector3(4.0, 0.0, 0.0))
 
 func set_bg_material(background_surface : MeshInstance3D, texture_path : String):
 	var image = Image.new()
@@ -39,7 +43,8 @@ func set_bg_material(background_surface : MeshInstance3D, texture_path : String)
 		# TODO May have to redefine the tiling? is there a way to fit relative to 256x256?
 		background_surface.material_override.uv1_scale = Vector3(10.0, 1.0, 1.0)
 
+func set_tilt(tilt_name):
+	tilt = tilt_name
 
-func tilt():
-	tilt_x = not tilt_x
-	pass # Replace with function body.
+func set_speed(speed_name):
+	scroll_command = speed_name
