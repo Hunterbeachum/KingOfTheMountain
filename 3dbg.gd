4a802f7e783e3e02bbc2261955eecf004eb7585e
Hunterbeachum <hsbeachum@my.waketech.edu>
2024-04-20 20:01:03 -0400

    Add 3D background, background scrolling, camera control

--- 3dbg.gd
+++ 3dbg.gd
@@ -1,27 +1,45 @@
 extends Node3D
-@onready var stairs = $Stairs
+var tilt : String = "top_down"
+var scroll_command : String = "mid"
+var scroll_speed : Vector3 = Vector3.ZERO
+const SCROLL_SPEED_LOW = Vector3(-0.1 * delta, 0.0, 0.0)
+const SCROLL_SPEED_MID = Vector3(-0.2 * delta, 0.0, 0.0)
+const SCROLL_SPEED_HIGH = Vector3(-0.4 * delta, 0.0, 0.0)
+const ANGLE_LOW = Vector3(50.0, 0.0, 0.0)
+const ANGLE_TOP = Vector3.ZERO
+@onready var bg_plane : MeshInstance3D = $bg_plane
+@onready var camera : Camera3D = $Camera
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var vertices = PackedVector3Array()
-	vertices.push_back(Vector3(0, 1, 0))
-	vertices.push_back(Vector3(1, 0, 0))
-	vertices.push_back(Vector3(0, 0, 1))
-
-	# Initialize the ArrayMesh.
-	var arr_mesh = ArrayMesh.new()
-	var arrays = []
-	arrays.resize(Mesh.ARRAY_MAX)
-	arrays[Mesh.ARRAY_VERTEX] = vertices
-
-	# Create the Mesh.
-	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-	var m = MeshInstance3D.new()
-	m.mesh = arr_mesh
-	add_child(m)
+	set_bg_material(bg_plane, "res://art/bg1.png")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if tilt == "top_down":
+		camera.rotation_degrees = camera.rotation_degrees().lerp(ANGLE_TOP, .1)
+	elif tilt == "low":
+		camera.rotation_degrees = camera.rotation_degrees().lerp(ANGLE_LOW, .1)
+	if scroll_command == "low":
+		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_LOW, .1)
+	elif scroll_command == "mid":
+		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_MID, .1)
+	elif scroll_command == "high":
+		scroll_speed = scroll_speed.lerp(SCROLL_SPEED_HIGH, .1)
+	bg_plane.transform(scroll_speed)
+
+func set_bg_material(background_surface : MeshInstance3D, texture_path : String):
+	var image = Image.new()
+	image.load(texture_path)
+	var texture = ImageTexture.create_from_image(image)
+	if background_surface.material_override:
+		background_surface.material_override.albedo_texture = texture
+		# TODO May have to redefine the tiling? is there a way to fit relative to 256x256?
+		background_surface.material_override.uv1_scale = Vector3(10.0, 1.0, 1.0)
+
+
+func tilt():
+	tilt_x = not tilt_x
+	pass # Replace with function body.
--- game/GameField.gd
+++ game/GameField.gd
@@ -9,11 +9,11 @@ func _ready():
 	var bg = load("res://game/3_dbg.tscn")
 	background_instance = bg.instantiate()
 	add_child(background_instance)
-	'''var player = load("res://game/player.tscn")
+	var player = load("res://game/player.tscn")
 	player_instance = player.instantiate()
 	add_child(player_instance)
 	var boss = load("res://game/boss.tscn")
 	boss_instance = boss.instantiate()
-	add_child(boss_instance)'''
+	add_child(boss_instance)
 	
 	pass # Replace with function body.
